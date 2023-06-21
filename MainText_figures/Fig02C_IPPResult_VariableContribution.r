a10156968f9453cd38129f2624eb1e29fcc4b9f0
Vinicius Peripato <65520358+Vperipato@users.noreply.github.com>
2023-06-21 12:24:36 -0300

    Update Fig02C_IPPResult_VariableContribution.r

--- MainText_figures/Fig02C_IPPResult_VariableContribution.r
+++ MainText_figures/Fig02C_IPPResult_VariableContribution.r
@@ -1,7 +1,6 @@
 rm(list = ls(globalenv()))
 gc()
 removeTmpFiles(h=0)
-dev.off()
 options(encoding = "UTF-8") 
 library(bayesplot)
 library(raster)
@@ -9,7 +8,6 @@ library(rgdal)
 library(viridis)
 library(ggmap)
 library(mapview)
-library(RStoolbox)
 library(ggspatial)
 library(sf)
 library(stringr)
@@ -20,52 +18,58 @@ library(gtools)
 library(dplyr)
 library(extrafont)
 
-setwd("F:/LiDAR_Data/Shapefiles/RegLogistica_Variables/")
-## Contribuição individual de cada variavel ========================================================================
-df_contrib = read.csv('./Modelo_Guido/ModelOutputs/00_IPPModel_CovarianceImportance.csv')
-df_contrib$vari = gsub("Intensity", "Intensity", df_contrib$vari)
-df_contrib$vari = gsub("Observability", "Obs.", df_contrib$vari)
+# library(devtools)
+# install_github("GuidoAMoreira/bayesPO")
+library(bayesPO)
 
-df_contrib$variable = gsub("1. Mean temperature\nwarmest quarter",
-                           "01. Mean temperature of warmest quarter", df_contrib$variable)
-df_contrib$variable = gsub("2. Precipitation\nwettest quarter",
-                           "02. Precipitation of wettest quarter", df_contrib$variable)
-df_contrib$variable = gsub("3. Precipitation\ndriest quarter",
-                           "03. Precipitation of driest quarter", df_contrib$variable)
-df_contrib$variable = gsub("4. Soil cation\nconcentration",
-                           "04. Soil cation concentration", df_contrib$variable)
-df_contrib$variable = gsub("5. Clay",
-                           "05. Clay content", df_contrib$variable)
-df_contrib$variable = gsub("6. Silt",
-                           "06. Silt content", df_contrib$variable)
-df_contrib$variable = gsub("7. TPI",
-                           "07. Topographic Position Index", df_contrib$variable)
-df_contrib$variable = gsub("8. HAND",
-                           "08. Height Above Nearest Drainage", df_contrib$variable)
-df_contrib$variable = gsub("9. Water\ndistance",
-                           "09. Water distance", df_contrib$variable)
-df_contrib$variable = gsub("1. Roads\ndistance",
-                           "10. Roads distance", df_contrib$variable)
-df_contrib$variable = gsub("2. Tree\ncover",
-                           "11. Tree cover", df_contrib$variable)
+setwd("F:/ade2541-manuscript/Database")
 
+# List .rds files of IPP model output data
+files = list.files(".", pattern = '^IPP_.*.rds$', full.names = T)
+po_est_all = read_rds(files, refhook = NULL)
+# cov_importance = po_est_all$covariates_importance
+rm(files)
+# 1 - Simple plot ##################################################################
+# par(mfrow = c(1, 2))
+# boxplot(po_est_all$covariates_importance[[1]]) # Intensity variables
+# boxplot(po_est_all$covariates_importance[[2]]) # Observability variables
+# par(mfrow = c(1, 1))
+
+# 2 - Formated plot #################################################################
+# 2.1 - Format the data.frames ######################################################
+# Intensity Covariate Importance
+df_intens = data.frame(po_est_all$covariates_importance[c('intensity')])
+names(df_intens) = c('8-HAND', 
+                     '1-Temp. warmest quarter',
+                     '2-Precip. wettest quarter',
+                     '3-Precip. driest quarter',
+                     '5-Clay content',
+                     '6-Silt content',
+                     '7-TPI',
+                     '4-SCC',
+                     '9-Distance to river')
+df_intens = melt(df_intens)
+df_intens$vari = 'Intensity'
+
+# Observability Covariate Importance
+df_obs = data.frame(po_est_all$covariates_importance[c('observability')])
+names(df_obs) = c('10-Distance to road',
+                  '11-Tree cover')
+df_obs = melt(df_obs)
+df_obs$vari = 'Obs.'
+
+# 2.2 - Merge Intensity & Observability data ######################################################
+df_contrib = rbind(df_intens, df_obs)
+rm(df_intens, df_obs, po_est_all)
+
+# Sort plot order
 df_contrib$o = factor(df_contrib$variable)
-levels(df_contrib$o) = levels(factor(sapply(str_split(df_contrib$o, "\\. "), "[", 1)))
+levels(df_contrib$o) = levels(factor(sapply(str_split(df_contrib$o, "-"), "[", 1)))
 df_contrib$variable_numb = as.numeric(df_contrib$o) 
-
-df_contrib$variable_name = sapply(str_split(df_contrib$variable, "\\. "), "[", 2)
+df_contrib$variable_name = sapply(str_split(df_contrib$variable, "-"), "[", 2)
 df_contrib$variable_name = factor(df_contrib$variable_name)
 
-
-df_contrib$variable_name = gsub("Mean temperature of warmest quarter",
-                                "Mean temp. of warmest quarter", df_contrib$variable_name)
-
-df_contrib$variable_name = gsub("Water distance",
-                                "Distance to nearest river", df_contrib$variable_name)
-
-df_contrib$variable_name = gsub("Roads distance",
-                                "Distance to nearest road", df_contrib$variable_name)
-
+# 2.3 ggplot #####################################################################################
 p_varicontrib =
   ggplot(df_contrib, aes(x = value, y = reorder(variable_name, desc(variable_numb)))) +
   geom_boxplot(fill='#440154', width = .95, lty = 1, lwd = 0.05,
@@ -86,25 +90,21 @@ p_varicontrib =
         axis.ticks = element_line(color="black", size = 0.2),
         axis.ticks.length.x = unit(2, "pt"),
         axis.ticks.length.y = unit(0, "pt"),
-        strip.text = element_text(size = 5, color = "black",
-                                    margin = margin(0, 0, 0, 1.5, "pt"), vjust = 0.5, hjust = 0.5),
+        strip.text = element_text(size = 7, family = 'Arial Narrow', color = "black", 
+                                  margin = margin(0, 0, 0, 1.5, "pt"), vjust = 0.5, hjust = 0.5),
         strip.background = element_rect(size=0.25, color = "black"),
-        plot.subtitle = element_text(size = 5, face = 'bold', margin = margin(3, 0, 2, 0, "pt")),
+        plot.subtitle = element_text(size = 8, family = 'Arial Narrow', face = 'bold', margin = margin(2, 0, 1, 0, "pt")),
         panel.grid.minor = element_line(size = 0.1),
         panel.grid.major = element_line(size = 0.2),
         panel.border = element_rect(fill = NA, size=0.25, color = "black"),
-        axis.text.x = element_text(size = 5, colour = 'black', margin = margin(0, 1, 0, 0, "pt")),
-        axis.text.y = element_text(size = 4.5, colour = 'black', margin = margin(0, .5, 0, 0, "pt")),
-        # axis.title.x = element_text(size = 4.5, face = "bold", margin = margin(0, 0, 0, 0, "pt")),
+        axis.text.x = element_text(size = 6, family = 'Arial Narrow', colour = 'black', vjust = 0.5, hjust = 0.5, margin = margin(1, 0, .5, 0, "pt")),
+        axis.text.y = element_text(size = 6, family = 'Arial Narrow', colour = 'black', margin = margin(0, .5, 0, 0, "pt")),
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         legend.title = element_blank(),
         legend.position = "none")
 
-# x11()
-# print(p_varicontrib)
-
-savepath_p4 = str_c("./Modelo_Guido/ModelOutputs/IPPModel04_VariableContribution.png")
-ggsave(savepath_p4, p_varicontrib, width = 4.45, height = 3.85,  units = "cm", dpi = 400, bg = 'white')
+savepath = str_c("./rplots/IPPModel03_VariableContribution.pdf")
+ggsave(savepath, p_varicontrib, width = 4.45, height = 3.85,  units = "cm", dpi = 400, 
+       bg = 'transparent', device = cairo_pdf)
 dev.off()
-
